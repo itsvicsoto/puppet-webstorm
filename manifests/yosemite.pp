b3a6e85ebbe8c79c304546b3a8befafa7a1cd401
itsvicsoto <itsvicsoto@gmail.com>
2015-07-02 15:56:58 +0800

    update(yosemite): updated yosemite class'

--- manifests/yosemite.pp
+++ manifests/yosemite.pp
@@ -1,7 +1,8 @@
-class webstormYosemite (
-) {
-  package { 'WebStormYosemite':
+#	include webstorm:yosemite
+
+class webstorm::yosemite {
+  package { 'WebStorm':
     provider => 'appdmg',
-    source   => "http://download.jetbrains.com/webstorm/WebStorm-10.0.4-custom-jdk-bundled.dmg"
+    source   => 'http://download.jetbrains.com/webstorm/WebStorm-10.0.4-custom-jdk-bundled.dmg',
   }
 }
