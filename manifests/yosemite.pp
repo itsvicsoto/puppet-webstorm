f6d1f5f3c54731e628899e827970734e3e756f85
itsvicsoto <itsvicsoto@gmail.com>
2015-07-02 16:17:55 +0800

    bugfix(yosemite): updated yosemite

--- manifests/yosemite.pp
+++ manifests/yosemite.pp
@@ -1,7 +1,7 @@
-#	include webstorm:yosemite
+# include webstorm::yosemite
 
 class webstorm::yosemite {
-  package { 'WebStorm':
+  package { 'WebStorm Yosemite':
     provider => 'appdmg',
     source   => 'http://download.jetbrains.com/webstorm/WebStorm-10.0.4-custom-jdk-bundled.dmg',
   }
