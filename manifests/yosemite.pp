898b9eb1b6318693b588c8225d40ace776fd98f5
itsvicsoto <itsvicsoto@gmail.com>
2015-07-02 16:19:58 +0800

    bugfix(link): url for yosemite source

--- manifests/yosemite.pp
+++ manifests/yosemite.pp
@@ -3,6 +3,6 @@
 class webstorm::yosemite {
   package { 'WebStorm Yosemite':
     provider => 'appdmg',
-    source   => 'http://download.jetbrains.com/webstorm/WebStorm-10.0.4-custom-jdk-bundled.dmg',
+    source   => "http://download.jetbrains.com/webstorm/WebStorm-10.0.4-custom-jdk-bundled.dmg",
   }
 }
