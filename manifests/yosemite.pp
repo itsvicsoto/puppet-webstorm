76a9b397d1131f51504bb3fc86c7c9427a499ed6
itsvicsoto <itsvicsoto@gmail.com>
2015-07-03 08:35:15 +0800

    update(provider): updated to appdmg

--- manifests/yosemite.pp
+++ manifests/yosemite.pp
@@ -2,7 +2,7 @@
 
 class webstorm::yosemite {
   package { 'WebStorm Yosemite':
-    provider => 'pkgdmg',
+    provider => 'appdmg',
     source   => "https://download.jetbrains.com/webstorm/WebStorm-10.0.4-custom-jdk-bundled.dmg",
   }
 }
